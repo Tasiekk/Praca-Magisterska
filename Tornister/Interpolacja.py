5b4fc5c9c6d3a34a106adf99440c7945cb3a2b54
Tasiek <37037797+Tasiekk@users.noreply.github.com>
2018-04-03 12:40:21 +0200

    nowe pliki
    - napisany program
    - brak porownania ze szkolami

--- Tornister/Interpolacja.py
+++ Tornister/Interpolacja.py
@@ -5,13 +5,13 @@ Created on Fri Mar  9 10:06:30 2018
 @author: Tasiek
 """
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 def interpolation(x,y,degree):
 #    plt.scatter(x, y)
 #    xp = np.linspace(2014, 2018, 100)
     z = np.polyfit(x, y, degree)
     p = np.poly1d(z)
-#    plt.plot(x,y,".",xp,p(xp))
+#    plt.plot(x,y,".",xp,p(xp),'-')
 #    plt.show()
     return p(2018)
 
